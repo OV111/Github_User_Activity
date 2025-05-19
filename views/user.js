d85a1bbd92332f0f0ba9b296624ca587a0700955
Vahe <ohanyanv703@gmail.com>
2025-05-19 22:52:19 +0400

    views folder (with html,css,js)

---
+++ views/user.js
@@ -0,0 +1,5 @@
+const searchInput = document.getElementById("searchInput")
+const searchBtn = document.getElementById("searchBtn")
+searchBtn.addEventListener("click",() => {
+    console.log(searchInput.value)
+})
