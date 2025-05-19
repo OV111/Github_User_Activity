8af5c95678249905ce2c84adea48bfc78764146a
Vahe <ohanyanv703@gmail.com>
2025-05-19 23:37:36 +0400

    fetchusername func

--- views/user.js
+++ views/user.js
@@ -1,5 +1,19 @@
+// import { error } from "console"
+
 const searchInput = document.getElementById("searchInput")
 const searchBtn = document.getElementById("searchBtn")
-searchBtn.addEventListener("click",() => {
-    console.log(searchInput.value)
-})
+const fetchUserName = async () => {
+    const username = searchInput.value.trim()
+    if(!username) {
+        console.log("Pls write User Name!")
+        return ;
+    }
+    
+    const response = await fetch(`http://127.0.0.1:5500/search?username=${username}`)
+    if(!response.ok) {
+        throw new Error("HTTP Error", response.status)
+    }
+    const data = await response.json()
+    console.log(data)
+}
+searchBtn.addEventListener("click",fetchUserName)
